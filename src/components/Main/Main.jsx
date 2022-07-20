7251402666040b5fb8fa808895ce78bbee6d35f7
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-07-20 03:07:44 +0300

    card

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,13 @@
 import "./Main.scss";
 import { data } from "../../helpers/data";
+import Card from "./Card";
 
 const Main = () => {
-  console.log(data);
   return (
     <div className="card-container">
-      {data.map((item) => {
-        const { id, title, image, desc } = item;
-        return (
-          <div className="cards">
-            <h1>{title}</h1>
-            <img src={image} alt="" />
-            <p> {desc} </p>
-          </div>
-        );
-      })}
+      {data.map((item) => (
+        <Card {...item} key={item.id} />
+      ))}
     </div>
   );
 };
